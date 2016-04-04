338aa30960d4c2c5abf58f26b3ea993ef382733d
Nazarí González <nazari.nz@gmail.com>
2016-04-04 20:50:04 +0100

    fixed the definitions file

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,13 @@
-export declare module 'obj-pool'{
-  export interface poolOptions {
-    args?:any[];
-    amount?:number;
-  }
+export interface poolOptions {
+  args?:any[];
+  amount?:number;
+}
 
-  export class ObjPool<T>{
-    constructor(object:T, options?:poolOptions);
-    alloc():T;
-    free(object:T):void;
-    generate(amount:number):void;
-    clear():void;
-    length:number;
-  }
+export default class ObjPool<T>{
+  constructor(object:T, options?:poolOptions);
+  alloc():T;
+  free(object:T):void;
+  generate(amount:number):void;
+  clear():void;
+  length:number;
 }
